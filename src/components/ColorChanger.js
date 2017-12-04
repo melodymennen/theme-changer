873fee2d2bdfca33ab78d5e48c39eff9c8b65bb6
Melody <Melody@Melodys-MacBook-Pro.local>
2017-12-04 14:36:07 -0700

    step 6 continuation

--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -4,15 +4,18 @@ export default class ColorChanger extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      allowEdit: this.props.allowEdit
+      allowEdit: props.allowEdit
     };
   }
-  // componentWillReceiveProps
+
+  componentWillReceiveProps(nextProps){
+    this.setState({allowEdit:nextProps.allowEdit})
+  }
 
   render() {
     const { update } = this.props
     return (
-      <select className="dropDownContainer" onChange={event => update(event.target.value)}>
+      <select className="dropDownContainer" onChange={event => update(event.target.value)} disabled={this.state.allowEdit === 'false'}>
         <option value="black"> Black </option>
         <option value="blue"> Blue </option>
         <option value="green"> Green </option>
--- src/components/EditToggle.js
+++ src/components/EditToggle.js
@@ -4,7 +4,7 @@ export default class EditToggle extends Component {
   render() {
     const {update} = this.props;
     return (
-      <select className="dropDownContainer ml0">
+      <select className="dropDownContainer ml0" onChange={event => this.update(event.target.value)}>
         <option value="true"> Allow Edit </option>
         <option value="false"> Disable Edit </option>
       </select>
